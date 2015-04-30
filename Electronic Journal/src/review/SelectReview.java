e21a48637726cf0718c4ce871aad5957089a531e
Xibo Wang <paulop5288@gmail.com>
2015-04-30 15:30:00 +0100

    Rename package

---
+++ Electronic Journal/src/review/SelectReview.java
@@ -0,0 +1,72 @@
+package review;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.DBConnection;
+import review.*;
+
+public class SelectReview extends HttpServlet {
+	private String articleID = "";
+	public SelectReview() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		articleID = req.getParameter("article");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body>");
+		out.println("The article selected is article " + articleID);
+		out.println("</body></html>");
+		System.out.println("loaded.");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+	public static List<Article> getArticlesForReview() {
+		DBConnection dbConnection = new DBConnection();
+		PreparedStatement pstm = null;
+		List<Article> articles = new ArrayList<Article>();
+		try {
+			pstm = dbConnection.createPreparedStatement("select * from article order by submissionDate ASC;");
+			ResultSet resultSet = dbConnection.executeQuery(pstm);
+			for (int i = 0; i < 5; i++) {
+				if (resultSet.next()) {
+					String articleID = resultSet.getString("articleID");
+					String authorID = resultSet.getString("authorID");
+					String title = resultSet.getString("title");
+					String articleAbstract = resultSet.getString("abstract");
+					articles.add(new Article(articleID, authorID, title, articleAbstract));
+				}
+			}			
+			dbConnection.closeConnection();			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(articles);
+		return articles;
+	}
+	
+	public static void main(String[] args) {
+		SelectReview review = new SelectReview();
+		review.getArticlesForReview();
+	}
+}
---
+++ Electronic Journal/src/review/SubmitReview.java
@@ -0,0 +1,73 @@
+package review;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import database.*;
+
+
+public class SubmitReview extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1575704856800198225L;
+	private String reviewerLevel = "", articleAbstract = "", contribution = "", badPoints = "", errors = "";
+	private int  authorID, articleID;
+	public SubmitReview() {
+		super();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.err.println("fail to load driver.");
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		// should get from user
+		authorID = 1;
+		articleID = 1;
+		
+		// review form
+		reviewerLevel = req.getParameter("level");
+		articleAbstract = req.getParameter("abstract");
+		contribution = req.getParameter("contribution");
+		badPoints = req.getParameter("badpoint");
+		errors = req.getParameter("error");
+		
+		DBConnection dbConnection = new DBConnection();
+		PreparedStatement pstm = null;
+		try {
+			pstm = dbConnection.createPreparedStatement("INSERT INTO review VALUES (null,?,?,?,?,?,?,?);");
+			pstm.setInt(1, authorID);
+			pstm.setInt(2, articleID);
+			pstm.setString(3, reviewerLevel);
+			pstm.setString(4, articleAbstract);
+			pstm.setString(5, contribution);
+			pstm.setString(6, badPoints);
+			pstm.setString(7, errors);
+			
+			int updateResult = dbConnection.executeUpdate(pstm);
+			System.out.println(updateResult);
+			dbConnection.closeConnection();			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		out.println("<html><body>");
+		out.println(reviewerLevel);
+		out.println(articleAbstract);
+		out.println(contribution);
+		out.println(badPoints);
+		out.println(errors);
+		out.println("</body></html>");
+	}
+}
