eb6624e096db656ba7c17b581b38c231449907bd
Xibo Wang <paulop5288@gmail.com>
2015-05-04 01:04:34 +0100

    Modify Login

--- Electronic Journal/src/LoginServlet.java
+++ Electronic Journal/src/LoginServlet.java
@@ -3,6 +3,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import review.SelectReview;
+
 import java.sql.*;
 
 public class LoginServlet extends HttpServlet {
@@ -27,19 +29,26 @@ public class LoginServlet extends HttpServlet {
 		}
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
+		String role = req.getParameter("role");
 		String query = "select * from author where" + " username = '" + username
 				+ "' and " + "password = '" + password + "';";
-
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet resultSet = null;
 		boolean userIsMatched = false;
+		boolean isAuthor = true;
 		String lastName = null;
 		String organisation = null;
 		String title = null;
+		int userID = 0;
 		PrintWriter out = res.getWriter();
+		if (role != null && role.equalsIgnoreCase("reviewer")) {
+			isAuthor = false;
+		} else if (role != null && role.equalsIgnoreCase("author")) {
+			isAuthor = true;
+		}
 		//Start session management here
-		HttpSession session = req.getSession(true);
+		HttpSession session = req.getSession();
 
 		try {
 			con = DriverManager.getConnection(
@@ -52,8 +61,16 @@ public class LoginServlet extends HttpServlet {
 				lastName = resultSet.getString("lastName");
 				organisation = resultSet.getString("organisation");
 				title = resultSet.getString("title");
+				userID = resultSet.getInt("authorID");
 				userIsMatched = true;
+				session.setMaxInactiveInterval(30);	// 30 s
 				session.setAttribute("username",resultSet.getString("username"));
+				if (isAuthor) {
+					session.setAttribute("role", "author");
+				} else {
+					session.setAttribute("role", "reviewer");
+				}
+				
 			} else {
 				userIsMatched = false;
 			}
@@ -79,18 +96,31 @@ public class LoginServlet extends HttpServlet {
 			}
 		}
 		if (userIsMatched) {
-			res.setContentType("text/html");
-			
-			out.println("<html><body>");
-			out.println("Hello " + title + "." + lastName + ", welcome back to International Journal of Software Engineering");
-			out.println("</body></html>");
-			System.out.println("sucessful.");
+			if (!isAuthor && SelectReview.isReviewer(userID)) {
+				res.setContentType("text/html");
+				out.println("<html><body>");
+				out.println("Hello " + title + "." + lastName + ", welcome back to International Journal of Software Engineering"
+						+ "\nYou are logged in as a reviewer.");
+				out.println("</body></html>");
+				Cookie userName = new Cookie("username", username);
+	            userName.setMaxAge(30);
+	            res.addCookie(userName);
+				res.sendRedirect("SelectReview.jsp");
+				System.out.println("sucessful. reviewer");
+			} else if (isAuthor) {
+				// for author
+			} else if (!isAuthor) {
+				res.setContentType("text/html");
+				out.println("<html><body>");
+				out.println("Login failed: You are not a reviewer.");
+				out.println("</body></html>");
+				System.out.println("failed. reviewer");
+			}			
 		} else {
-			res.setContentType("text/html");
-			out.println("<html><body>");
-			out.println("Login failed: Invalid username or password.");
-			out.println("</body></html>");
-			System.out.println("failed.");
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/reviewer.jsp");
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            System.out.println("invalid");
+            rd.include(req, res);
 		}
 	}
 }
